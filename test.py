efe5043d144ca05b464503414e2c204a9d15af13
stephane-robin <robin.stephane@outlook.com>
2020-04-17 20:01:56 +0200

    working on code

---
+++ test.py
@@ -0,0 +1,16 @@
+import xmltodict  # interprets XML files like JSON files
+import subprocess  # allows to connect to input/output/error pipes of processes
+from os import mkdir, chdir, remove, listdir, rename, getcwd, system, \
+    stat  # system
+from pickle import load, dump
+from xlrd import open_workbook
+from openpyxl import load_workbook
+import csv
+import shutil  # TESTER UTILITE D'IMPORTER TOUT LE MODULE
+from shutil import copyfile
+from Bio import pairwise2, SeqIO
+from Bio.pairwise2 import format_alignment
+from Bio import Entrez  # provides code to access NCBI over the Web
+from datetime import datetime
+
+print("hello")
